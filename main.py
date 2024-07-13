28a3c5a60e9583dac8e3ad1e7368c89f459e4aea
Abdur Rehman Siddiqui <abisbah2011@gmail.com>
2024-07-13 10:03:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+a = "Peel" #This is to show how to print instead of using for loop, slicing and indexing
+if "P" in a: # If "p" is in a it will print the folowing line
+ print("P is for point")
+if "e" in a[1]: # Since this is not elif it wil check if there is e on 1 index it will print the line
+  print("E is for evidence")
+if "e" in a[2]: # It will check e on 2 index and will print the line 
+  print("E is for explanation")
+if "l" in a: # lastly it will check l and will print the line
+  print("L stands for Link")
+else: #else
+  print("Invalid word entered")
+b = "Method"
+for c in b:
+  print(c)
+
+
+  
